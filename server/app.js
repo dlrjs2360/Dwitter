6c6c10abaca4980d58d4d04489e3658060e406e5
dlrjs2360 <lhg3670@naver.com>
2021-09-01 15:43:06 +0900

    beauty apply

--- server/app.js
+++ server/app.js
@@ -5,9 +5,9 @@ import morgan from 'morgan';
 import helmet from 'helmet';
 import tweetsRouter from './router/tweets.js';
 import authRouter from './router/auth.js';
-import { config } from './config.js';
-import { initSocket } from './connection/socket.js';
-import {sequelize } from './db/database.js';
+import {config} from './config.js';
+import {initSocket} from './connection/socket.js';
+import {sequelize} from './db/database.js';
 
 const app = express();
 
@@ -20,14 +20,16 @@ app.use('/tweets', tweetsRouter);
 app.use('/auth', authRouter);
 
 app.use((req, res, next) => {
-  res.sendStatus(404);
+    res.sendStatus(404);
 });
 
 app.use((error, req, res, next) => {
-  console.error(error);
-  res.sendStatus(500);
+    console.error(error);
+    res.sendStatus(500);
 });
-sequelize.sync().then(() => {
-    const server = app.listen(config.host.port);
-    initSocket(server)
-  })
+sequelize
+    .sync()
+    .then(() => {
+        const server = app.listen(config.host.port);
+        initSocket(server)
+    })
